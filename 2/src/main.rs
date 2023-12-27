5ccfd592c084fef7071e0967a0e77051af32a3d8
Stuart Leitch <Toruitas@gmail.com>
2023-12-27 13:31:44 +0000

    2 works

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::collections::HashMap;
+use std::cmp::max;
 
 fn main() {
     let mut final_answer:u32 = 0;
@@ -17,7 +18,8 @@ fn main() {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(game) = line {
-                final_answer += playable_game(game, &max_cubes);
+                // final_answer += playable_game(game, &max_cubes);
+                final_answer += playable_game_part_2(game);
             }
         }
     }
@@ -31,22 +33,18 @@ fn main() {
 fn playable_game(game: String, max_cubes: &HashMap<&str, u32>) -> u32{
     // split on ":" and the 1st substring has the game ID
     let game_id_and_games = game.trim().split(":").collect::<Vec<&str>>();
-    println!("{:?}", game_id_and_games);
 
     // split substring 1 on " " and the 2nd value is the game ID
     let id_str = game_id_and_games[0].trim().split(" ").collect::<Vec<&str>>();
-    println!("{:?}", id_str);
 
     // let id =  id_str[1].parse::<u32>().unwrap();
     let id:u32 = match id_str[1].trim().parse(){
         Ok(num) => num,
         Err(e) => panic!("Can't convert to u32: {e}"),
     };
-    println!("{:?}", id);
 
     // split substring 2 by ";" to get the 'hand'
     let hands = game_id_and_games[1].trim().split(";").collect::<Vec<&str>>();
-    println!("{:?}", hands);
     
     // for each, split by "," to get number and color
     for hand in hands {
@@ -56,23 +54,59 @@ fn playable_game(game: String, max_cubes: &HashMap<&str, u32>) -> u32{
             // split by space " " and idx 0 is the num while 1 is the color
             let color_and_count = color.trim().split(" ").collect::<Vec<&str>>();
             // if color count is greater than the max for that color, return 0
-            println!("{:?}", color_and_count);
-            let count_int: u32 = match color_and_count[0].trim().trim().parse(){
+            let count_int: u32 = match color_and_count[0].trim().parse(){
                 Ok(num) => num,
                 Err(e) => panic!("Can't convert to u32: {e}"),
             };
-            println!("{:?}", count_int);
-
-            println!("{:?}", max_cubes.get(color_and_count[1]).cloned().unwrap_or(0));
 
             if max_cubes.get(color_and_count[1]).cloned().unwrap_or(0) < count_int{
-                println!("{:?}", 0);
                 return 0;
             }
         }
     };
 
     return id;
+}
+
+
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+fn playable_game_part_2(game: String) -> u32{
+    // split on ":" and the 1st substring has the game ID
+    let game_id_and_games = game.trim().split(":").collect::<Vec<&str>>();
+
+    // split substring 2 by ";" to get the 'hand'
+    let hands = game_id_and_games[1].trim().split(";").collect::<Vec<&str>>();
+
+    let mut min_red: u32 = 0;
+    let mut min_green: u32 = 0;
+    let mut min_blue: u32 = 0;
+    
+    // for each, split by "," to get number and color
+    for hand in hands {
+        let hand_colors = hand.trim().split(",").collect::<Vec<&str>>();
+
+        for color in hand_colors{
+            // split by space " " and idx 0 is the num while 1 is the color
+            let color_and_count = color.trim().split(" ").collect::<Vec<&str>>();
+            // if color count is greater than the max for that color, return 0
+            let count_int: u32 = match color_and_count[0].trim().parse(){
+                Ok(num) => num,
+                Err(e) => panic!("Can't convert to u32: {e}"),
+            };
+
+            if color_and_count[1].trim() == "red"{
+                min_red = max(min_red, count_int)
+            }
+            else if color_and_count[1].trim() == "green"{
+                min_green = max(min_green, count_int)
+            }
+            else{
+                min_blue = max(min_blue, count_int)
+            }
+        }
+    };
+
+    return min_red * min_green * min_blue;
 }
 
 
